8dea73dea00a2cecc744ab5123bd50f7219a14e3
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-14 23:59:32 +0500

    Adding more commands in to the program and creating a text file for all the commands

--- src/sqlite3/connection.py
+++ src/sqlite3/connection.py
@@ -1,3 +1,15 @@
 import sqlite3 
 
 conn = sqlite3.connect("tutorial.db")
+
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE movie(title, year, score)")
+
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
+
+conn.commit()
